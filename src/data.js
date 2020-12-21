754e83d97aad2276ff3fa47ddbfe7b2a494f5bb5
francisca abbey blay <fablay002@st.ug.edu.gh>
2020-12-21 14:39:09 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,54 @@
+
+
+export default {
+    products: [
+        {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/p1.webp',
+        price:'40',
+        brand: 'Nike',
+        rating: 4.2,
+        numReviews: 10
+    } ,
+{
+    name: 'Slim pants',
+    category:'pants',
+    image: '/images/p1.webp',
+    price:'40',
+    brand: 'Nike',
+    rating: 4.2,
+    numReviews: 10
+} ,
+
+{
+    name: 'large Shirt',
+    category: 'Shirts',
+    image: '/images/p1.webp',
+    price:'40',
+    brand: 'Nike',
+    rating: 4.2,
+    numReviews: 10
+} ,
+
+{
+    name: 'loose pants',
+    category: 'pants',
+    image: '/images/p1.webp',
+    price:'40',
+    brand: 'Nike',
+    rating: 4.2,
+    numReviews: 10
+} ,
+
+{
+    name: 'casual Shirt',
+    category: 'Shirts',
+    image: '/images/p1.webp',
+    price:'40',
+    brand: 'Nike',
+    rating: 4.2,
+    numReviews: 10
+ } ,
+    ]
+}
